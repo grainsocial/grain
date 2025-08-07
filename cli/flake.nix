7c1f2b31b3ab1d561a28c3eb9514ce5dd5bb6184
Chad Miller <chadtmiller15@gmail.com>
2025-08-06 20:12:21 -0700

    fix: use crane instead of rust-overlay in nix flake

--- cli/flake.nix
+++ cli/flake.nix
@@ -4,42 +4,24 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
+    crane = {
+      url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils, crane }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
-        
-        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [ "rust-src" ];
-          targets = [
-            "x86_64-unknown-linux-gnu"
-            "x86_64-pc-windows-gnu" 
-            "x86_64-apple-darwin"
-            "aarch64-apple-darwin"
-            "aarch64-unknown-linux-gnu"
-          ];
-        };
+        pkgs = import nixpkgs { inherit system; };
 
-        # Build function for different targets
-        buildGrainCLI = target: pkgs.stdenv.mkDerivation rec {
-          pname = "grain-cli-${target}";
-          version = "0.1.0";
-          
-          src = ./.;
-          
-          nativeBuildInputs = with pkgs; [
-            rustToolchain
-            pkg-config
-          ] ++ pkgs.lib.optionals (target == "x86_64-pc-windows-gnu") [
-            pkgs.pkgsCross.mingwW64.stdenv.cc
-          ];
+        craneLib = crane.mkLib pkgs;
+
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
           
           buildInputs = with pkgs; [
             openssl
@@ -48,25 +30,24 @@
             pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
           ];
 
-          buildPhase = ''
-            export CARGO_HOME=$TMPDIR/cargo
-            export OPENSSL_NO_VENDOR=1
-            export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
-            
-            ${if target == "x86_64-pc-windows-gnu" then ''
-              export CC_x86_64_pc_windows_gnu="${pkgs.pkgsCross.mingwW64.stdenv.cc}/bin/x86_64-w64-mingw32-gcc"
-              export CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER="${pkgs.pkgsCross.mingwW64.stdenv.cc}/bin/x86_64-w64-mingw32-gcc"
-              export PKG_CONFIG_ALLOW_CROSS=1
-            '' else ""}
-            
-            cargo build --release --target ${target}
-          '';
-          
-          installPhase = ''
-            mkdir -p $out/bin
-            cp target/${target}/release/grain${if target == "x86_64-pc-windows-gnu" then ".exe" else ""} $out/bin/
-          '';
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
         };
+
+        # Build dependencies first for caching
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        # Build function for different targets
+        buildGrainCLI = target: craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+          CARGO_BUILD_TARGET = target;
+        } // pkgs.lib.optionalAttrs (target == "x86_64-pc-windows-gnu") {
+          depsBuildBuild = with pkgs; [
+            pkgsCross.mingwW64.stdenv.cc
+          ];
+          CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER = "${pkgs.pkgsCross.mingwW64.stdenv.cc}/bin/x86_64-w64-mingw32-gcc";
+        });
 
       in {
         packages = {
@@ -83,7 +64,8 @@
         devShells.default = pkgs.mkShell {
           inputsFrom = [ self.packages.${system}.default ];
           packages = with pkgs; [
-            rustToolchain
+            rustc
+            cargo
             pkg-config
             openssl
             cargo-cross
